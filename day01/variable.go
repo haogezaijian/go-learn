e528622a2ac80dcfd4844ef6de546071cdfa3def
haoge <1668173696@qq.com>
2024-06-06 19:32:02 +0800

    go 变量

---
+++ day01/variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//声明变量的一般形式是使用 var 关键字
+
+var a int = 10 //声明一个int类型的变量
+
+// 这种写法一般用于声明全局变量
+var (
+	b   int    = 20
+	c   bool   = false
+	str string = "str"
+)
+
+// 声明局部变量,函数体内
+func getNum() int {
+	a := 10 //优先使用函数体内的局部变量
+	return a
+}
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(str)
+
+	fmt.Println(getNum())
+}
