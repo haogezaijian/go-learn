d9486b4878b7eeffc46c8793d4ac6f403c81c75b
haoge <1668173696@qq.com>
2024-06-06 16:55:39 +0800

    hello world

--- day01/hello_world.go
+++ day01/hello_world.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import fm "fmt" //给引入的包起别名
 
+//导入多个包
+/*
+	type1
+	import "fmt"
+	import "os"
+*/
+/*
+	type2
+	import "fmt";import "os"
+*/
+/**
+	type3
+	import(
+	"fmt"
+	"os"
+)
+*/
+/*
+	type4
+	import("fmt";"os")
+*/
 func main() {
-	fmt.Println("Hello World")
+	//导入包如果未使用会报错
+	fm.Println("Hello World")
 }
